1abf2ee37a5f76853623cad113ee6f593c647c91
Anubrata-01 <anubratachanda36@gmail.com>
2024-06-23 17:40:47 +0530

    activate the trending books funtionality

--- src/Pages/Trending.jsx
+++ src/Pages/Trending.jsx
@@ -1,8 +1,18 @@
-
+import BookCard from "../books/BookCard";
+import { getAllBooks } from "../books/book"
 
 const Trending = () => {
+  const books=getAllBooks();
+  const trending_books=books.filter((book)=> book?.rating === 5);
+
   return (
-    <div className=" text-white absolute left-80 top-20">Trending</div>
+    <div className=" w-[700px] h-[500px] border-2 text-white absolute left-80 top-20">
+      <div className=" relative w-full h-full grid sm:grid-cols-3 xl:grid-cols-4 gap-4 pl-4 overflow-y-scroll">
+        {trending_books?.map((book, index) => (
+          <BookCard key={index} book={book} />
+        ))}
+      </div>
+    </div>
   )
 }
 
